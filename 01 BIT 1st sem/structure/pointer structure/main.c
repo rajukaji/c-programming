2f2a8f8cae7f754d4e5d0ad04e438984923bae9c
Raju Lama <rajulamarainon@gmail.com>
2024-03-09 00:04:55 +0545

    pointer, structure, and dynamic memory allocation

--- 01 BIT 1st sem/structure/pointer structure/main.c
+++ 01 BIT 1st sem/structure/pointer structure/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+//dynamic memory allocation of structs
 struct details{
     int id;
     char name[20];
@@ -6,10 +7,23 @@ struct details{
 
 int main()
 {
-    struct details *pt1, *pt2;
-    pt1 = &details->id;
-    pt2 = &details->name;
-    printf("Name :: %s\n", details->*ptr1);
-    printf("Id:: %d \n", details->*ptr2);
+    struct details *ptr;
+    int i, n;
+    printf("Enter number of persons :: ");
+    scanf("%d", &n);
+    ptr = (struct details*) malloc(n * sizeof(struct details));//allocating memory dynamically
+    printf("Enter names of ::\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d :: ", i + 1);
+        scanf(" %[^\n]", &(ptr + i)->name);//need & for the address of each element of pointer array
+        //here space in the beginning of scanf is must
+        printf("\n");
+    }
+    printf("The details are :: \n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d :: %s\n", i + 1, (ptr + i)->name);//no dereferencing needed
+    }
     return 0;
 }
