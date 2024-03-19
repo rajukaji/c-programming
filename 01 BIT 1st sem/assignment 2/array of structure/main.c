bdf8d5a01f8f481099bf7d74bef1bbcb3cbaa19f
Raju Lama <rajulamarainon@gmail.com>
2024-03-20 00:12:41 +0545

    output

--- 01 BIT 1st sem/assignment 2/array of structure/main.c
+++ 01 BIT 1st sem/assignment 2/array of structure/main.c
@@ -9,12 +9,12 @@ struct details{
     char f_name[15];
     char l_name[15];
     char address[20];
-    long long int mobileNo;
+    unsigned long long int mobileNo;
  };
 
 int main()
 {
-    struct details employee[10];
+    struct details employee[10];//array of structure
     int i;
     for(i = 0; i < 10; i++)
     {
@@ -24,11 +24,31 @@ int main()
         printf("Enter your last name ::  ");
         scanf("%s", &employee[i].l_name);
         printf("Enter your ESSN ::  ");
-        scanf("%s", &employee[i].essn);
+        scanf("%d", &employee[i].essn);
         printf("Enter your address ::  ");
         scanf(" %[^\n]", &employee[i].address);//leave space infront
         printf("Enter your mobile number ::  ");
         scanf("%llu", &employee[i].mobileNo);
     }
+
+    printf("The details you entered are :: \n\n");
+
+    for(i = 0; i < 10; i++)
+    {
+        printf("For employee %d :: \n", i + 1);
+
+        printf("  first name ::  %s\n", employee[i].f_name);
+
+        printf("  last name ::  %s\n", employee[i].l_name);
+
+        printf("  ESSN ID ::  %d\n", employee[i].essn);
+
+        printf("  Address ::  %s\n", employee[i].address);
+
+        printf("  Mobile number ::  %llu\n\n", employee[i].mobileNo);
+
+
+    }
+
     return 0;
 }
