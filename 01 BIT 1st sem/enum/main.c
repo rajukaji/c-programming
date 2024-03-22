30e50d440b2cb30146c3517971e9986a274e46ae
Raju Lama <rajulamarainon@gmail.com>
2024-03-23 00:15:57 +0545

    enum

--- 01 BIT 1st sem/enum/main.c
+++ 01 BIT 1st sem/enum/main.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 //enum
 enum week {
-    sunday,
-    monday,
-    tuesday,
-    thursday,
-    friday,
-    saturday
+    sunday,//value 0
+    monday,//value 1
+    tuesday,// value 2
+    thursday = 10,// initializing value 10
+    friday,// value 11
+    saturday = 20 //value 20
 };
 int main()
 {
@@ -15,8 +15,14 @@ int main()
     printf("day :: %d\n", day);//prints 0
     day = sunday;
     printf("day = sunday :: %d\n", day);//prints 0;
-    day.monday = 8;
+    //monday = 8; cannot do
     printf("day = Monday :: %d\n", day);
+    day = thursday;
+    printf("day = Monday :: %d\n", day);//prints 10
+    day = friday;
+    printf("day = Monday :: %d\n", day); //prints 11
+    day = saturday;
+    printf("day = Monday :: %d\n", day);//prints 20
 
     return 0;
 }
