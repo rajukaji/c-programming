d6c9b86a6ae74c078d7274ecb3ec183da86e01a1
Raju Lama <rajulamarainon@gmail.com>
2024-03-24 14:28:31 +0545

    fputs

--- 01 BIT 1st sem/file/fputs/main.c
+++ 01 BIT 1st sem/file/fputs/main.c
@@ -7,6 +7,13 @@ int main()
     file = fopen("fputs.txt", "w+");
     fputs("Hello world!\n", file);
     fputs("Sincere world!\n", file);
+
+    char sentence[1000];
+    printf("Enter a sentence :: \n");
+    scanf(" %[^\n]\n", sentence);
+    fputs(sentence, file);
+
+
     fclose(file);
     return 0;
 }
